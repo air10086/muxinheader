b0edf0c5a4096aaa69c4d044510892ce30fdbb4b
muxinhanwei <770811054@qq.com>
2020-07-04 17:29:37 +0800

    打包

--- config/webpack.base.js
+++ config/webpack.base.js
@@ -4,7 +4,7 @@ module.exports = {
       {
         test: /.(js|jsx)$/,
         exclude: /node_modules/,
-        loader: 'babel-loader',
+        loader: 'babel-loader?chaheDirectory',
         options: {
           plugins: [
             [
@@ -39,7 +39,7 @@ module.exports = {
           {
             loader: 'style-loader',
           },
-          { loader: 'css-loader' },
+          { loader: 'css-loader?modules' },
           {
             loader: 'less-loader',
             options: {
--- config/webpack.prod.config.js
+++ config/webpack.prod.config.js
@@ -2,6 +2,8 @@ const path = require('path')
 const merge = require('webpack-merge')
 const baseConfig = require('./webpack.base.js')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin') // 用于将组件的css打包成单独的文件输出到`lib`目录中
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin') // 压缩css代码
+const TerserPlugin = require('terser-webpack-plugin') // 压缩js代码
 
 const prodConfig = {
   mode: 'production',
@@ -25,6 +27,18 @@ const prodConfig = {
       filename: 'main.min.css', // 提取后的css的文件名
     }),
   ],
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: 4, // 开启几个进程来处理压缩，默认是 os.cpus().length - 1
+        cache: true, // 是否缓存
+        sourceMap: false,
+      }),
+
+      // 压缩css
+      new OptimizeCSSAssetsPlugin({}),
+    ],
+  },
   externals: {
     // 定义外部依赖，避免把react和react-dom打包进来
     react: {
@@ -33,6 +47,7 @@ const prodConfig = {
       commonjs: 'react',
       amd: 'react',
     },
+
     'react-dom': {
       root: 'ReactDOM',
       commonjs2: 'react-dom',
